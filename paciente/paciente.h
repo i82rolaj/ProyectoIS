631d3b3b0a36b57ad961c6cb5a09734f22d0c231
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2019-11-19 10:43:17 +0100

    Create paciente.h

---
+++ paciente/paciente.h
@@ -0,0 +1,24 @@
+#ifndef PACIENTES_H
+#define PACIENTES_H
+#include <string>
+
+using namespace std;
+
+class Paciente{
+private:
+	string nombreapellidos_;
+	string sexo_;
+	string fechanacimiento_;
+	string domicilio_;
+	int telefono_;
+	string procedencia_;
+public:
+	Paciente(string nombreapellidos,string sexo,string fechanacimiento,int telefono,string domicilio="",string procedencia="");
+	bool anadir_paciente();
+	bool borrar_paciente();
+	bool buscar_paciente();
+	bool modificar_paciente();
+	void listar_pacientes();
+};
+
+#endif
