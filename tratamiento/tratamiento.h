7c976f6210d327abe2b2f2f2800ed02dd8fe1b38
i82sarus <55401069+i82sarus@users.noreply.github.com>
2019-11-20 12:21:13 +0100

    Corrección clase tratamiento

--- tratamiento/tratamiento.h
+++ tratamiento/tratamiento.h
@@ -3,22 +3,19 @@
 
 #include <string>
 
+using namespace std;
+
 class Tratamiento{
 private:
+	string nombrepaciente_t_;
 	string diagnostico_;
 	string medicacion_;
 	string fecha_inicio_;
 	string fecha_fin_;//formato: 01/01/1900
 	string observaciones_;
 public:
-	inline Tratamiento(string diagnostico,string medicacion,string fecha_inicio="",string fecha_fin="",string observaciones=""){
-		diagnotico_=diagnostico;
-		medicacion_=medicacion;
-		fecha_inicio_=fecha_inicio;
-		fecha_fin_=fecha_fin;
-		observaciones_=observaciones;
-	}
-	void anadirTratamiento(string nombre);
+	Tratamiento(string nombrepaciente_t,string diagnostico,string medicacion,string fecha_inicio,string fecha_fin,string observaciones);
+	bool anadir_tratamiento();
 };
 
 #endif
