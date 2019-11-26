5e9a4f27b178ea8bcd8ed9df52923a1bb531db15
I82ROLAJ <55401132+I82ROLAJ@users.noreply.github.com>
2019-11-26 11:23:42 +0100

    Update paciente.h

--- paciente/paciente.h
+++ paciente/paciente.h
@@ -15,7 +15,7 @@ private:
 public:
 	Paciente(string nombreapellidos,string sexo,string fechanacimiento,int telefono,string domicilio="",string procedencia="");
 	bool anadir_paciente();
-	bool borrar_paciente();
+	bool borrar_paciente(string nombrecompleto);
 	bool modificar_paciente();
 	void listar_pacientes();
 
